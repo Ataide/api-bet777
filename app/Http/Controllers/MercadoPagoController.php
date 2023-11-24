44afcb86b4f9c78604acf4ac50afd691f2aee942
Ataide Bastos <ataide.bastos@gmail.com>
2023-11-24 13:33:33 -0300

    implementacao do meio de pagamento pix no mercado pago

--- app/Http/Controllers/MercadoPagoController.php
+++ app/Http/Controllers/MercadoPagoController.php
@@ -13,18 +13,25 @@ class MercadoPagoController extends Controller
         MercadoPagoConfig::setAccessToken(env("MP_ACCESS_TOKEN"));
 
         $action = $request->input('action');
+        
+        \Log::debug(print_r($action, 1));
+
         \Log::debug(print_r($request->all(), 1));
 
         switch($action) {
             case "state_FINISHED":
                 $payment = Payment::find_by_id($_POST["data"]["id"]);
                 \Log::debug(print_r($payment, 1));
+              
+                // no break
+            case "payment.created":
+                echo "payment.created";
+                $payment = Payment::find_by_id($_POST["data"]["id"]);
+                \Log::debug(print_r($payment, 1));
 
                 break;
-           
-            case "point_integration_wh":
-                // $_POST contém as informações relacionadas à notificação.
-                break;
         }
+
+        return response()->json(['status' => 200], 200);
     }
 }
